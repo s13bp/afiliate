af44ebbd0ff2d18b977cf44b59e7ec5482a8de66
s13bp <86206902+s13bp@users.noreply.github.com>
2022-05-01 16:52:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,4 +16,19 @@ const pages = document.querySelectorAll(".page");
         pages => (pages.style.transform = `translateX(${translate}%)`)
       );
     }
-   
+    dropContainer.ondragover = dropContainer.ondragenter = function(evt) {
+      evt.preventDefault();
+    };
+    
+    dropContainer.ondrop = function(evt) {
+      // pretty simple -- but not for IE :(
+      fileInput.files = evt.dataTransfer.files;
+    
+      // If you want to use some of the dropped files
+      const dT = new DataTransfer();
+      dT.items.add(evt.dataTransfer.files[0]);
+      dT.items.add(evt.dataTransfer.files[3]);
+      fileInput.files = dT.files;
+    
+      evt.preventDefault();
+    };  
